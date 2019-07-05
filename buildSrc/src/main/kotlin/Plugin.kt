fbb6523719fa49e2dac15747f0e3c8ad17dbbccc
Björn Petersen <pheasn@gmail.com>
2019-07-05 22:06:04 +0200

    Update Kotlin to 1.3.41

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -2,7 +2,7 @@ object Plugin {
     const val SPOTLESS = "3.23.1"
     const val VERSIONS = "0.21.0"
 
-    const val KOTLIN = "1.3.40"
+    const val KOTLIN = "1.3.41"
     const val DOKKA = "0.9.18"
     const val SHADOW_JAR = "5.1.0"
 }
