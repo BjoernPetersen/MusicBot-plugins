b665992a928d8bf1ef267d668b073296d3f040fc
Björn Petersen <pheasn@gmail.com>
2019-08-31 17:18:05 +0200

    Document YouTubePlaybackFactory

--- musicbot-youtube/src/main/kotlin/net/bjoernpetersen/musicbot/youtube/playback/YouTubePlaybackFactory.kt
+++ musicbot-youtube/src/main/kotlin/net/bjoernpetersen/musicbot/youtube/playback/YouTubePlaybackFactory.kt
@@ -5,11 +5,30 @@ import net.bjoernpetersen.musicbot.spi.loader.Resource
 import net.bjoernpetersen.musicbot.spi.plugin.Playback
 import net.bjoernpetersen.musicbot.spi.plugin.PlaybackFactory
 
+/**
+ * PlaybackFactory for playing YouTube songs/videos.
+ */
 @Base
 interface YouTubePlaybackFactory : PlaybackFactory {
 
+    /**
+     * Loads the video with the specified ID.
+     *
+     * @param videoId a video ID
+     * @return a resource which resulted from loading
+     */
     suspend fun load(videoId: String): YouTubeResource
+
+    /**
+     * Creates a playback object using the specified resource.
+     *
+     * @param resource the resource which was created by [load]
+     * @return a playback object
+     */
     suspend fun createPlayback(resource: YouTubeResource): Playback
 }
 
+/**
+ * A resource for a specific YouTube video.
+ */
 abstract class YouTubeResource(val videoId: String) : Resource
