e0fe61a174b0437bfad3f3925960a2027caf52d3
Björn Petersen <pheasn@gmail.com>
2019-09-01 14:00:23 +0200

    Update bump script

--- scripts/bump.ps1
+++ scripts/bump.ps1
@@ -1,13 +1,26 @@
 Param (
     [parameter(Mandatory = $true)]
-    [String]
-    $Name
+    [String[]]
+    $Names,
+    [switch]
+    $Snapshot
 )
 
 . scripts/common.ps1
 
-$DirName = Get-DirName $Name
-$Version = Find-Version $DirName
+foreach ($Name in $Names) {
+    $DirName = Get-DirName $Name
+    [string]$Version = Find-Version $DirName
+    if ($Version.EndsWith("-SNAPSHOT")) {
+        $Version = $Version.Substring(0, $Version.Length - "-SNAPSHOT".Length)
+    }
 
-git add $DirName
-git commit -m "Update $Name to $Version"
+    [version]$V = $Version
+    $NewVersion = "{0}.{1}.{2}" -f $V.Major, ($V.Minor + 1), $V.Build
+    if ($Snapshot) {
+        $NewVersion = $NewVersion + "-SNAPSHOT"
+    }
+
+    git add $DirName
+    git commit -m "Update $Name to $Version"
+}
