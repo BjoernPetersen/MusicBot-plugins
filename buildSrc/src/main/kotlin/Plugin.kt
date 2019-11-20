a0a839f19991518244eed64063d8c0fd980f213e
Björn Petersen <pheasn@gmail.com>
2019-11-20 16:58:26 +0100

    Update Gradle to 6.0.1

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -3,7 +3,7 @@ object Plugin {
     const val DETEKT = "1.1.1"
     const val VERSIONS = "0.27.0"
 
-    const val KOTLIN = "1.3.50"
+    const val KOTLIN = "1.3.60"
     const val DOKKA = "0.10.0"
     const val SHADOW_JAR = "5.1.0"
 }
