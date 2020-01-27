77834de125775eb7dd00121c655822ce33ae85db
Björn Petersen <pheasn@gmail.com>
2020-01-27 19:42:31 +0100

    Update some dependencies

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -4,7 +4,7 @@ object Lib {
     const val MUSICBOT = "0.24.0-SNAPSHOT"
 
     const val SLF4J = "1.8.0-beta4"
-    const val JUNIT = "5.5.2"
+    const val JUNIT = "5.6.0"
 
     const val KTOR = "1.2.5"
 
@@ -14,18 +14,18 @@ object Lib {
     const val SQLITE = "3.28.0"
 
     // mpv
-    const val NU_PROCESS = "1.2.4"
+    const val NU_PROCESS = "1.2.6"
 
     // Radio
     const val M3U_PARSER = "1.0.0"
 
     // Spotify
-    const val SPOTIFY = "4.2.0"
+    const val SPOTIFY = "4.2.1"
 
     // sysvolume
     const val VOLCTL = "3.0.0"
 
     // YouTube
     const val YOUTUBE_API = "v3-rev20190827-1.30.1"
-    const val CAFFEINE = "2.8.0"
+    const val CAFFEINE = "2.8.1"
 }
--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,8 +1,8 @@
 object Plugin {
-    const val SPOTLESS = "3.26.0"
+    const val SPOTLESS = "3.27.1"
     const val DETEKT = "1.1.1"
     const val VERSIONS = "0.27.0"
 
-    const val KOTLIN = "1.3.60"
+    const val KOTLIN = "1.3.61"
     const val SHADOW_JAR = "5.2.0"
 }
