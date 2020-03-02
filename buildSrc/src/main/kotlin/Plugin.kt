e06cb08ae03667da6f4060120d0aaa5170d6d8e8
Björn Petersen <pheasn@gmail.com>
2020-03-02 19:38:42 +0100

    Update plugins

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,7 +1,7 @@
 object Plugin {
     const val SPOTLESS = "3.27.1"
-    const val DETEKT = "1.5.0"
-    const val VERSIONS = "0.27.0"
+    const val DETEKT = "1.6.0"
+    const val VERSIONS = "0.28.0"
 
     const val KOTLIN = "1.3.61"
     const val SHADOW_JAR = "5.2.0"
