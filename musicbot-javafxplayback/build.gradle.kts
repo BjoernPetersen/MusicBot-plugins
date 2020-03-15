7e8c6336132c8bd1bcb9023a20c191283634fa42
Björn Petersen <pheasn@gmail.com>
2020-03-15 16:29:34 +0100

    Update javafxplayback to 0.15.0

--- musicbot-javafxplayback/build.gradle.kts
+++ musicbot-javafxplayback/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version Plugin.SHADOW_JAR
 }
 
-version = "0.14.0"
+version = "0.15.0"
 
 tasks {
     val shadowJar by getting(ShadowJar::class) {
