647523ded31bb4e9102fd159984cdcad98325e6e
Björn Petersen <pheasn@gmail.com>
2020-11-08 22:12:39 +0100

    Flush after writing

--- musicbot-mpv/src/main/kotlin/net/bjoernpetersen/musicbot/mpv/control/UnixPipe.kt
+++ musicbot-mpv/src/main/kotlin/net/bjoernpetersen/musicbot/mpv/control/UnixPipe.kt
@@ -13,16 +13,16 @@ class UnixPipe(
     private val lock = ReentrantLock()
     private val socketAddress = AFUNIXSocketAddress(path.toFile())
     private val socket = AFUNIXSocket.newInstance().also {
-        println(path)
         it.connect(socketAddress)
-        println("Connected")
         it.keepAlive = true
-        println("Keeping alive")
     }
     private val inputReader = socket.inputStream.bufferedReader()
     private val outputWriter = socket.outputStream.bufferedWriter()
 
     override fun readLine(): String? {
+        if(!inputReader.ready()) {
+            return null
+        }
         return inputReader.readLine() ?: throw EOFException()
     }
 
@@ -30,6 +30,7 @@ class UnixPipe(
         val terminated = if (line.endsWith('\n')) line else "$line\n"
         lock.withLock {
             outputWriter.write(terminated)
+            outputWriter.flush()
         }
     }
 
